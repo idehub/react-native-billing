8fe82dfaff9d5b5099ca5ae96a05af8f5e2ea631
Austin Kettner <austinkettner@users.noreply.github.com>
2018-01-17 01:08:53 -0500

    Improved Callback and Error Handling (#86)
    * Resolve promise directly from `onActivityResult`
    
    * Better handle activity result
    
    Bump to version 2.4.2
    
    * Revert package version to 2.4.0 as per idehub request

--- android/src/main/java/com/idehub/Billing/InAppBillingBridge.java
+++ android/src/main/java/com/idehub/Billing/InAppBillingBridge.java
@@ -24,6 +24,7 @@ import java.util.List;
 import java.util.Map;
 
 public class InAppBillingBridge extends ReactContextBaseJavaModule implements ActivityEventListener, BillingProcessor.IBillingHandler {
+
     ReactApplicationContext _reactContext;
     String LICENSE_KEY = null;
     BillingProcessor bp;
@@ -407,15 +408,18 @@ public class InAppBillingBridge extends ReactContextBaseJavaModule implements Ac
         return BillingProcessor.isIabServiceAvailable(_reactContext);
     }
 
-    @Deprecated
-    public void onActivityResult(final int requestCode, final int resultCode, final Intent intent) {
-        if (bp != null)
-            bp.handleActivityResult(requestCode, resultCode, intent);
-    }
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        if (requestCode != PromiseConstants.PURCHASE_FLOW_REQUEST_CODE) {
+            return;
+        }
 
-    public void onActivityResult(final Activity activity, final int requestCode, final int resultCode, final Intent intent) {
-        if (bp != null)
-            bp.handleActivityResult(requestCode, resultCode, intent);
+        int responseCode = data.getIntExtra(PromiseConstants.RESPONSE_CODE, PromiseConstants.BILLING_RESPONSE_RESULT_OK);
+        if (resultCode == Activity.RESULT_OK && responseCode == PromiseConstants.BILLING_RESPONSE_RESULT_OK) {
+            resolvePromise(PromiseConstants.PURCHASE_OR_SUBSCRIBE, true);
+        } else {
+            rejectPromise(PromiseConstants.PURCHASE_OR_SUBSCRIBE, "An error has occured. Code " + requestCode);
+        }
     }
 
     @Override
