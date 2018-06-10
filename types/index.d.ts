db3bb64fca911e951f4a4401dc343bac06812986
Christian Brevik <christian.brevik@gmail.com>
2018-06-10 15:23:31 +0200

    Refactor types + add linting/check

---
+++ types/index.d.ts
@@ -0,0 +1,94 @@
+// TypeScript Version: 2.4
+
+export enum PurchaseState {
+  PurchasedSuccessfully = "PurchasedSuccessfully",
+  Canceled = "Canceled",
+  Refunded = "Refunded",
+  SubscriptionExpired = "SubscriptionExpired"
+}
+
+export interface ITransactionDetails {
+  productId: string;
+  orderId: string;
+  purchaseToken: string;
+  purchaseTime: string;
+  purchaseState: PurchaseState;
+  receiptSignature: string;
+  receiptData: string;
+  developerPayload: string;
+}
+
+export interface IProductDetails {
+  productId: string;
+  title: string;
+  description: string;
+  isSubscription: string;
+  currency: string;
+  priceValue: string;
+  priceText: string;
+}
+
+export interface ISubscriptionDetails extends IProductDetails {
+  subscriptionPeriod: string;
+  subscriptionFreeTrialPeriod?: string;
+  haveTrialPeriod: boolean;
+  introductoryPriceValue: number;
+  introductoryPriceText?: string;
+  introductoryPricePeriod?: string;
+  haveIntroductoryPeriod: boolean;
+  introductoryPriceCycles: number;
+}
+
+export default class InAppBilling {
+  open(): Promise<void>;
+
+  close(): Promise<void>;
+
+  purchase(
+    productId: string,
+    developerPayload?: string
+  ): Promise<ITransactionDetails>;
+
+  consumePurchase(
+    productId: string,
+    developerPayload?: string
+  ): Promise<ITransactionDetails>;
+
+  isSubscribed(productId: string): Promise<boolean>;
+
+  updateSubscription(
+    oldProductIds: string[],
+    productId: string,
+    developerPayload?: string
+  ): Promise<ITransactionDetails>;
+
+  isPurchased(productId: string): Promise<boolean>;
+
+  isOneTimePurchaseSupported(): Promise<boolean>;
+
+  isValidTransactionDetails(productId: string): Promise<boolean>;
+
+  listOwnedProducts(): Promise<string[]>;
+
+  listOwnedSubscriptions(): Promise<string[]>;
+
+  getProductDetails(productId: string): Promise<IProductDetails>;
+
+  getProductDetailsArray(productIds: string[]): Promise<IProductDetails[]>;
+
+  getSubscriptionDetails(productId: string): Promise<ISubscriptionDetails>;
+
+  getSubscriptionDetailsArray(
+    productIds: string[]
+  ): Promise<ISubscriptionDetails[]>;
+
+  getPurchaseTransactionDetails(
+    productId: string
+  ): Promise<ITransactionDetails>;
+
+  getSubscriptionTransactionDetails(
+    productId: string
+  ): Promise<ITransactionDetails>;
+
+  loadOwnedPurchasesFromGoogle(): Promise<any>;
+}
